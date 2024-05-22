37b65497733c990c166a178582bc84ddc3acb503
NTThanh2711 <thanhbct5abtx@gmail.com>
2024-05-22 11:26:51 +0700

    change footer 1th

--- OnDemandTutor/src/components/Footer/Footer.tsx
+++ OnDemandTutor/src/components/Footer/Footer.tsx
@@ -1,31 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export default function Footer() {
-  return <div>
-    <>
+  return (
         <div className='Footer-container'>
           <div className="F-content">
             <div className='F-Left'> 
               <div>ODT Learning</div>
-              <div>ảnh nè</div>
+              <div>
+                  <a href=''><i className='fa fa-facebook' aria-hidden='true'></i></a>
+                  <a href=''></a>
+                  <a href=''></a>
+              </div>
             </div> 
             <div className='F-Right'> 
                <ul className='F-R-C'>
                 <li>Resource</li>
-                <li><a href=''>Home</a></li>
-                <li><a href=''>Become a tutor</a></li>
-                <li><a href=''>Find tutor</a></li>
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/'}>Become a tutor</Link></li>
+                <li><Link to={'/'}>Find tutor</Link></li>
                </ul>
                <ul className='F-R-C'>
                 <li>Tutoriton</li>
-                <li><a href=''>Featured tutor</a></li>
-                <li><a href=''>About us</a></li>
-                <li><a href=''>Support</a></li>
+                <li><Link to={'/'}>Featured tutor</Link></li>
+                <li><Link to={'/'}>About us</Link></li>
+                <li><Link to={'/'}>Support</Link></li>
                </ul>
                <ul className='F-R-C'>
                 <li>References</li>
-                <li><a href=''>Contact us</a></li>
-                <li><a href=''>Blog</a></li>
-                <li><a href=''>Page</a></li>
+                <li><Link to={'/'}>Contact us</Link></li>
+                <li><Link to={'/'}>Blog</Link></li>
+                <li><Link to={'/'}>Page</Link></li>
                </ul>
             </div>
           </div>
@@ -34,7 +38,6 @@ export default function Footer() {
           </div>
           
         </div>
-
-    </>
-  </div>
+    
+  )
 }
