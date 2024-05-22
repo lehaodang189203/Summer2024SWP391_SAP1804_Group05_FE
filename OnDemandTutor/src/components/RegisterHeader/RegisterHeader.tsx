8ab89ddca4e23aab4792f7f4bd07ba02fe88b931
thanhgithub123@gmail.com <thanhgithub123@gmail.com>
2024-05-22 23:42:15 +0700

    Chỉnh sửa 1 xíu form đăng kí và Header

--- OnDemandTutor/src/components/RegisterHeader/RegisterHeader.tsx
+++ OnDemandTutor/src/components/RegisterHeader/RegisterHeader.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from '../../assets/img/logo.png'
+import newLogo from '../../assets/img/newLogo.png'
 export default function RegisterHeader() {
   return (
     <header className='border-black border-2 mt-1 rounded-xl py-2 bg-white'>
@@ -10,50 +10,52 @@ export default function RegisterHeader() {
             <Link to='/'>
               <div className='  justify-end items-end flex '>
                 <div className=' w-32 pt-[100%] mb-10 relative '>
-                  <div className='absolute top-0 left-0  w-full h-full object-cover '>
-                    <img src={logo} alt='logo' />
+                  <div className='absolute top-0 left-0  w-[180px] h-full object-cover '>
+                    <img src={newLogo} alt='logo' />
                   </div>
                 </div>
               </div>
             </Link>
           </nav>
           {/*  input */}
-          <form className='col-span-6 pb-20 px-2'>
-            <div className='bg-white rounded-sm p-1 flex'>
-              <input
-                type='text'
-                placeholder='Tìm kím gia sư và các khóa học'
-                className='text-black px-3 py-2 flex-grow rounded-lg border-black border-2 outline-none bg-transparent'
-              />
-              {/* kính lúp*/}
-              <button className='rounded-lg mx-2 py-2 px-6 flex-shrink-0 bg-black text-white hover:opacity-90'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
-                  />
-                </svg>
-              </button>
+          <form className='col-span-7 pb-14 px-1'>
+            <div className=' border-gray-400 border-[2px] rounded-md hover:border-black hover:border-[5px]'>
+              <div className='bg-white  p-1 flex'>
+                <input
+                  type='text'
+                  placeholder='Tìm kím gia sư và các khóa học'
+                  className='text-black px-3 py-2 flex-grow rounded-lg  h-[4rem] outline-none bg-transparent'
+                />
+                {/* kính lúp*/}
+                <button className='rounded-lg py-2 px-6 flex-shrink-0 border-l-0  text-black hover:opacity-90'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='w-6 h-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </form>
-          <div className='col-span-4 pb-24 justify-between items-center flex'>
-            <Link to='/' className='text-center'>
+          <div className='col-span-3 pb-24 justify-between items-center flex'>
+            <Link to='/' className='text-center font-bold hover:text-pink-600'>
               Trang chủ
             </Link>
 
-            <Link to='/' className='text-center'>
+            <Link to='/' className='text-center font-bold hover:text-pink-600'>
               Loại dịch vụ
             </Link>
 
-            <Link to='/' className='text-center'>
+            <Link to='/' className='text-center font-bold hover:text-pink-600'>
               Hỗ trợ
             </Link>
           </div>
--- OnDemandTutor/src/pages/Register/Register.tsx
+++ OnDemandTutor/src/pages/Register/Register.tsx
@@ -1,3 +1,5 @@
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -5,28 +7,53 @@ export default function Register() {
   return (
     <div className='bg-white py-5 w-full border-red-500 border-2'>
       <div className='container justify-center flex'>
-        <form style={{ background: '#F7F3F3' }} className='p-10  shadow-sm border-black border-2 w-4/12  rounded-2xl '>
+        <form
+          style={{ background: '#F7F3F3', boxShadow: 'rgba(0, 0, 0, 0.1) -18px 20px 4px 7px' }}
+          className='p-10 w-4/12  rounded-2xl shadow-neutral-950'
+        >
           <div className='text-2xl'>Đăng Ký</div>
           <div className='mt-8'>
             <input
               type='email'
               placeholder='Email'
-              className='p-3 w-full outline-none border border-gray-300 forcus:border-gray-500 forcus:shawdow-sm rounded-sm'
+              className='p-3 w-full outline-none  border-gray-300 forcus:border-gray-500 forcus:shawdow-sm rounded-xl hover:border-black border-2'
             />
           </div>
           <div className='mt-3'>
             <input
               type='password'
-              placeholder='password'
-              className='p-3 w-full outline-none border border-gray-300 forcus:border-gray-500 forcus:shawdow-sm rounded-sm'
+              placeholder='Mật Khẩu'
+              className='p-3 w-full outline-none border-gray-300 forcus:border-gray-500 forcus:shawdow-sm rounded-xl  hover:border-black border-2'
             />
           </div>
           <div className='mt-3'>
-            <button className='w-full  rounded-xl text-center bg-blue-500 py-4 px-2 uppercase  text-white text-sm hover:bg-blue-300 flex justify-center items-center'>
-              Đăng nhập
+            <input
+              type='password'
+              placeholder='Xác nhận mật Khẩu'
+              className='p-3 w-full outline-none border-gray-300 forcus:border-gray-500 forcus:shawdow-sm rounded-xl  hover:border-black border-2'
+            />
+          </div>
+          <div className='mt-3'>
+            <button className='w-full  rounded-xl text-center bg-pink-300 py-4 px-2 uppercase  text-white text-sm hover:bg-pink-600 flex justify-center items-center'>
+              Đăng Ký
             </button>
           </div>
-          <div className='mt-8 '>
+
+          <div className='NleHE1'>
+            <span>---------------------------</span>
+            <span className='EMof35'>hoặc</span>
+            <span>---------------------------</span>
+          </div>
+
+          <div className='justify-center flex py-2'>
+            <button className=' bg-black text-white border-black border-2 w-[300px] rounded-lg justify-center items-center flex py-2 shadow-2xl hover:bg-white hover:text-black'>
+              <div className='pr-2'>
+                <FontAwesomeIcon icon={faGoogle} />
+              </div>
+              <div>Google</div>
+            </button>
+          </div>
+          <div className='my-4'>
             <div>
               <span className='text-gray-600 mr-1'>Bạn đã có tài khoảng?</span>
               <Link className='text-gray-500 underline hover:text-red-500' to='/Login'>
