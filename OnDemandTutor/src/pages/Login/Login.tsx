bfa112ee63553a7971be4ebcef7de7fe26549fd2
YuuSanLearnCode <theanhkip@gmail.com>
2024-05-29 18:33:21 +0700

    Edit the text in the login registration section, add packages to packages.jason

--- OnDemandTutor/src/pages/Login/Login.tsx
+++ OnDemandTutor/src/pages/Login/Login.tsx
@@ -60,7 +60,7 @@ export default function Login() {
               type='submit'
               className='w-full rounded-xl text-center bg-pink-300 py-4 px-2 uppercase text-white text-sm hover:bg-pink-600 flex justify-center items-center'
             >
-              Đăng Ký
+              Đăng Nhập
             </button>
           </div>
         </form>
@@ -81,12 +81,12 @@ export default function Login() {
         </div>
         <div className='my-4'>
           <div>
-            <span className='text-gray-600 mr-1'>Bạn đã có tài khoảng?</span>
+            <span className='text-gray-600 mr-1'>Bạn chưa có tài khoản? </span>
             <Link
               className='text-gray-500 underline hover:text-red-500'
               to='/Login'
             >
-              Đăng Nhập
+              Đăng Ký
             </Link>
           </div>
         </div>
