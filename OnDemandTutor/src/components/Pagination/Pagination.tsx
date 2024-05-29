5ae681a4b26943d54bde354cdd1338eb93228e1a
thanhgithub123@gmail.com <thanhgithub123@gmail.com>
2024-05-29 16:22:00 +0700

    Tạo Popover và thêm Navigation

---
+++ OnDemandTutor/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,147 @@
+import classNames from 'classnames'
+import { Link } from 'react-router-dom'
+import path from '../../constant/path'
+
+interface Props {
+  pageSize: number
+}
+
+/**
+Với range = 2 áp dụng cho khoảng cách đầu, cuối và xung quanh current_page
+
+[1] 2 3 ... 19 20
+1 [2] 3 4 ... 19 20 
+1 2 [3] 4 5 ... 19 20
+1 2 3 [4] 5 6 ... 19 20
+1 2 3 4 [5] 6 7 ... 19 20
+
+1 2 ... 4 5 [6] 8 9 ... 19 20
+
+1 2 ...13 14 [15] 16 17 ... 19 20
+
+
+1 2 ... 14 15 [16] 17 18 19 20
+1 2 ... 15 16 [17] 18 19 20
+1 2 ... 16 17 [18] 19 20
+1 2 ... 17 18 [19] 20
+1 2 ... 18 19 [20]
+ */
+
+const RANGE = 2
+export default function Pagination({ pageSize }: Props) {
+  let page = 1
+  const renderPagination = () => {
+    let dotAfter = false
+    let dotBefore = false
+
+    const renderDotBefore = (index: number) => {
+      if (!dotBefore) {
+        dotBefore = true
+        return (
+          <span
+            key={index}
+            className='mx-2 rounded border bg-white px-3 py-2 shadow-sm'
+          >
+            ...
+          </span>
+        )
+      }
+      return null
+    }
+    const renderDotAfter = (index: number) => {
+      if (!dotAfter) {
+        dotAfter = true
+        return (
+          <span
+            key={index}
+            className='mx-2 rounded border bg-white px-3 py-2 shadow-sm'
+          >
+            ...
+          </span>
+        )
+      }
+      return null
+    }
+    return Array(pageSize)
+      .fill(0)
+      .map((_, index) => {
+        const pageNumber = index + 1
+
+        // Điều kiện để return về ...
+        if (
+          page <= RANGE * 2 + 1 &&
+          pageNumber > page + RANGE &&
+          pageNumber < pageSize - RANGE + 1
+        ) {
+          return renderDotAfter(index)
+        } else if (page > RANGE * 2 + 1 && page < pageSize - RANGE * 2) {
+          if (pageNumber < page - RANGE && pageNumber > RANGE) {
+            return renderDotBefore(index)
+          } else if (
+            pageNumber > page + RANGE &&
+            pageNumber < pageSize - RANGE + 1
+          ) {
+            return renderDotAfter(index)
+          }
+        } else if (
+          page >= pageSize - RANGE * 2 &&
+          pageNumber > RANGE &&
+          pageNumber < page - RANGE
+        ) {
+          return renderDotBefore(index)
+        }
+
+        return (
+          <Link
+            to={path.tutorList}
+            key={index}
+            className={classNames(
+              'mx-2 cursor-pointer rounded border bg-white px-3 py-2 shadow-sm',
+              {
+                'border-cyan-500': pageNumber === page,
+                'border-transparent': pageNumber !== page
+              }
+            )}
+          >
+            {pageNumber}
+          </Link>
+        )
+      })
+  }
+
+  return (
+    <div className='mt-6 flex flex-wrap justify-center'>
+      {page === 1 ? (
+        <span className='mx-2 cursor-not-allowed rounded border bg-white/60 px-3 py-2  shadow-sm'>
+          Prev
+        </span>
+      ) : (
+        <Link
+          to={path.tutorList}
+          className='mx-2 cursor-pointer rounded border bg-white px-3 py-2  shadow-sm'
+        >
+          Prev
+        </Link>
+      )}
+
+      {renderPagination()}
+
+      {page === pageSize ? (
+        <span className='mx-2 cursor-not-allowed rounded border bg-white/60 px-3 py-2  shadow-sm'>
+          Next
+        </span>
+      ) : (
+        //  tại sao chúng ta ưu tiên thẻ Link hơn là button
+        //  chúng ta có thể thấy được url
+        // or chúng ta có thể open Link in new Tab
+        // còn dùng button or navigate thì không dùng đc như z
+        <Link
+          to={path.tutorList}
+          className='mx-2 cursor-pointer rounded border bg-white px-3 py-2  shadow-sm'
+        >
+          Next
+        </Link>
+      )}
+    </div>
+  )
+}
--- OnDemandTutor/src/components/RegisterHeader/RegisterHeader.tsx
+++ OnDemandTutor/src/components/RegisterHeader/RegisterHeader.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import BUMBUM from '../../assets/img/BUMBUM.png'
+import Popover from '../Popover/Popover'
 
 export default function RegisterHeader() {
   return (
@@ -19,14 +20,33 @@ export default function RegisterHeader() {
           {/* Mid */}
           <div className='h-36 col-span-6 text-2xl'>
             <div className='pr-[50px] pt-[40px] justify-around items-center flex'>
-              <Link to='#' className='ct-top-menu-items'>
+              <Link to='/' className='ct-top-menu-items'>
                 Trang chủ
               </Link>
 
-              <Link to='/' className='ct-top-menu-items'>
-                Loại dịch vụ
-              </Link>
-
+              <Popover
+                className='flex items-center py-1  cursor-pointer shadow-red-700'
+                renderPopover={
+                  <div className='shadow-red-700'>
+                    <div className='  w-[20rem] flex mt-0.5   items-center justify-between text-center  text-[10px]  px-auto rounded-3xl'>
+                      <Link
+                        to='/registerAT'
+                        className='py-2 w-[10rem] h-full  bg-pink-400 border-pink-400 border-2 text-black  rounded-l-3xl   hover:text-white'
+                      >
+                        Đăng ký trở thành giảng viên
+                      </Link>
+                      <Link
+                        to='/tutorList'
+                        className='py-2 w-[10rem]  h-full  text-white  bg-black border-black border-2  rounded-r-3xl  hover:text-pink-400'
+                      >
+                        danh sách lớp
+                      </Link>
+                    </div>
+                  </div>
+                }
+              >
+                <div className='ct-top-menu-items'>Loại dịch vụ</div>
+              </Popover>
               <Link to='/' className='ct-top-menu-items'>
                 Hỗ trợ
               </Link>
@@ -34,14 +54,19 @@ export default function RegisterHeader() {
           </div>
           {/* Login */}
           <nav className='h-36 pt-[48px] col-span-3'>
-            <Link to='/login' className='transition duration-150 ease-in-out border-black border-2 px-2 py-2 rounded-lg hover:bg-gray-200 mr-1 '>
+            <Link
+              to='/login'
+              className='transition duration-150 ease-in-out border-black border-2 px-2 py-2 rounded-lg hover:bg-gray-200 mr-1 '
+            >
               Đăng Nhập
             </Link>
-            <Link to='/register' className='bg-pink-500 border-black border-2 px-2 py-2 rounded-lg hover:bg-pink-400'>
+            <Link
+              to='/register'
+              className='bg-pink-500 border-black border-2 px-2 py-2 rounded-lg hover:bg-pink-400'
+            >
               Đăng kí
             </Link>
           </nav>
-          
         </div>
       </div>
     </header>
